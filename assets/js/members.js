3488de6b650831d2f1c30db673f2d9928044f55c
Mike Green <m.d.j.green@ntlworld.com>
2020-07-14 14:05:15 +0000

    POC POST members to external file

--- assets/js/members.js
+++ assets/js/members.js
@@ -22,9 +22,17 @@ function loadMember() {
   console.log("******POST  *******");
   xmlhttp = new XMLHttpRequest();
   xmlhttp.open("POST", memberFile, true);
-  xmlhttp.setRequestHeader("Content-type", "text/json");
+  xmlhttp.setRequestHeader("Content-type", "application/text");
   console.log("***** "+memberJSON);
   xmlhttp.send(memberJSON);
+  xmlhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             document.getElementById("memberData").innerHTML = this.memberJSON;
+        } else {
+            console.log("******** state "+ this.readyState +" ******* status " +this.status);
+        };   
+  }
+  
 }
 
 //loadMember();
@@ -37,7 +45,8 @@ function getMembers(cb) {
     xhr.send();
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(JSON.parse(this.responseText));
+             document.getElementById("memberData").innerHTML = this.responseText;
+            //console.log(JSON.parse(this.responseText));
         } else {
             console.log("******** state "+ this.readyState +" ******* status " +this.status);
         };
@@ -45,7 +54,7 @@ function getMembers(cb) {
 }
 function writeToDocument() {
     getMembers(function(data){
-        document.getElementById("data").innerHTML=data;
+        document.getElementById("memberData").innerHTML=data;
     });
 }
 
