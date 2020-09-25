afbd048016d3755b08d2194675efb7f9ca9c328b
Mike Green <m.d.j.green@ntlworld.com>
2020-09-25 11:09:48 +0000

    more work with selection validation and object detail display

--- assets/js/metart.js
+++ assets/js/metart.js
@@ -1,95 +1,102 @@
 // Setting up api portal
 const apiMet = "https://collectionapi.metmuseum.org/public/collection/v1/";
-const apiMetObject = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+const apiMetObject =
+  "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
 
 // Declaring global variables used by more than one function
 
-var searchCrit1 = "departmentID=";
-var searchCrit2 = "q=sunflower";
-var qryStr ="";  // q
-var qryHighlight;   // isHighlight
-var qryDept;        // departmentId
-var qryView;        // isOnView
-var qryCult;        // artistOrCulture
-var qryMedium;     // medium
-var qryImages;       // hasImages
-var qryLoc;          // geoLocation
+var searchCrit1 = "";
+var searchCrit2 = "";
+var qryStr = ""; // q
+var qryHighlight; // isHighlight
+var qryDept; // departmentId
+var qryView; // isOnView
+var qryCult; // artistOrCulture
+var qryMedium; // medium
+var qryImages; // hasImages
+var qryLoc; // geoLocation
 // must have both values for dateBegin and dateEnd queries:
-var qryBegin;        // dateBegin
-var qryEnd;          // dateEnd
+var qryBegin; // dateBegin
+var qryEnd; // dateEnd
 var deptName; //department Name
-var totalObjects;   // to capture the total number of objects listed on the Met's public collectsions
-
+var totalObjects; // to capture the total number of objects listed on the Met's public collectsions
 
 function getTotalObjects(cb) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET",apiMetObject);
-    xhr.send();
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {  
-            cb(JSON.parse(this.responseText));
-            console.log("********  JSON response text "+JSON.parse(this.responseText));
-        } else {
-            console.log("******** state "+ this.readyState +" ******* status " +this.status);
-        };
-    };
-};
+  var xhr = new XMLHttpRequest();
+  var apiAll = apiMetObject.substring(0, apiMetObject.length - 1);
+  xhr.open("GET", apiAll);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
+      console.log(
+        "********  JSON response text " + JSON.parse(this.responseText)
+      );
+    }
+  };
+}
 
 function totalCollection() {
-    getTotalObjects(function(item) {
-       totalObjects = item.total;
-       console.log(`There are a total collection of ${totalObjects} objects` );
-       return totalObjects;
-    })
-};
-
-
+  getTotalObjects(function (item) {
+    totalObjects = item.total;
+    document.getElementById(
+      "metArtTotal"
+    ).innerHTML = `There is a total of ${totalObjects} objects in the Met's collection.`;
+    return totalObjects;
+  });
+}
 
 function getMetDept(cb) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET",apiMet + "departments");
-    xhr.send();
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {  
-            cb(JSON.parse(this.responseText));
-            console.log("********  JSON response text "+JSON.parse(this.responseText));
-        } else {
-            console.log("******** state "+ this.readyState +" ******* status " +this.status);
-        };
-    };
-};
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", apiMet + "departments");
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
+      console.log(
+        "********  JSON response text " + JSON.parse(this.responseText)
+      );
+    } else {
+      console.log(
+        "******** state " + this.readyState + " ******* status " + this.status
+      );
+    }
+  };
+}
 
 function writeDepts() {
-    getMetDept(function(item) {
-       var depts = [];
-       depts=item.departments;
-       depts.forEach(function(item) {
-                document.getElementById("metArtDept").innerHTML += item.departmentId+") "+item.displayName+" <br>";
-            });
+  getMetDept(function (item) {
+    var depts = [];
+    depts = item.departments;
+    depts.forEach(function (item) {
+      document.getElementById("metArtDept").innerHTML +=
+        item.departmentId + ") " + item.displayName + " <br>";
     });
-};
+  });
+}
 
 function writeDeptName(data) {
-    document.getElementById("metCriteria").innerHTML += " : "+ data.displayName + "</p>";
+  document.getElementById("metCriteria").innerHTML +=
+    " : " + data.displayName + "</p>";
 }
 
 function getDeptName(deptId) {
-    var depts = [];
-    getMetDept(function(item) {
-        depts=item.departments;
-        depts.forEach(function(item) {
-              if ( item.departmentId == deptId ){
-                  writeDeptName(item);
-              }
-        });
+  var depts = [];
+  getMetDept(function (item) {
+    depts = item.departments;
+    depts.forEach(function (item) {
+      if (item.departmentId == deptId) {
+        writeDeptName(item);
+      }
     });
+  });
 }
 
 /*
     Initialising popovers to help with selection criteria validation, UX
 */
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('[data-toggle="popover"]').popover();
 });
 
@@ -101,161 +108,209 @@ $(document).ready(function(){
         The returned query also contains total number of objects found.
 */
 
-
-
-
-
 function getMetSearch(cb1) {
-    var xhr2 = new XMLHttpRequest();
-    xhr2.open("GET",apiMet + "search?"+searchCrit1+"&"+searchCrit2);
-    xhr2.send();
-    xhr2.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {  
-            cb1(JSON.parse(this.responseText));
-            //console.log("********  JSON response text "+JSON.parse(this.responseText));
-        };
-    };
-};
+  var xhr2 = new XMLHttpRequest();
+  xhr2.open("GET", apiMet + "search?" + searchCrit1 + searchCrit2);
+  xhr2.send();
+  xhr2.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb1(JSON.parse(this.responseText));
+      //console.log("********  JSON response text "+JSON.parse(this.responseText));
+    }
+  };
+}
 
 function getMetObject(obj_ID, cb2) {
- 
-    var xhr3 = new XMLHttpRequest();
-    xhr3.open("GET",apiMetObject + obj_ID);
-    xhr3.send();
+  var xhr3 = new XMLHttpRequest();
+  xhr3.open("GET", apiMetObject + obj_ID);
+  xhr3.send();
 
-    xhr3.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {  
-            cb2(JSON.parse(this.responseText));
-         };
-    };
-};
+  xhr3.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb2(JSON.parse(this.responseText));
+    }
+  };
+}
 
 function writeCriteria() {
-    document.getElementById("metCriteria").innerHTML = "<p> Search criteria: "+searchCrit1+" "+searchCrit2+" </p>";
-};
+  document.getElementById("metCriteria").innerHTML =
+    "<p> Search criteria: " + searchCrit1 + " " + searchCrit2 + " </p>";
+}
 
 function writeObjects() {
-    var objects = [];
-    var objectId;
+  var objects = [];
+  var objectId;
 
-    /*
+  /*
         Clear down previous search results...
     */
-   document.getElementById("metArt").innerHTML = "";
-    /*
+  document.getElementById("metArt").innerHTML = "";
+  /*
         Now for current search results.....
     */
-    writeCriteria();
-    getMetSearch(function(item) {
-       var total_Found;
-       total_Found = item.total;
-       document.getElementById("metArt").innerHTML += "<p> Total found: "+total_Found+" </p>";
-       objects=item.objectIDs;
-       
-       for (objectId of objects) {
-           //document.getElementById("metArt").innerHTML += "<p> "+objectId+" </p>";
-           writeObjectDetails(objectId);
-       };
+  writeCriteria();
+  getMetSearch(function (item) {
+    var total_Found;
+    total_Found = item.total;
+    document.getElementById("metArt").innerHTML +=
+      "<p> Total found: " + total_Found + " </p>";
+    objects = item.objectIDs;
 
-    });
-};
+    for (objectId of objects) {
+      //document.getElementById("metArt").innerHTML += "<p> "+objectId+" </p>";
+      writeObjectDetails(objectId);
+    }
+  });
+}
 
 function writeObjectDetails(obj_ID) {
-    var objTitle = "";
-    var objPrimaryImage ="";
-    var objArtistDisplayName ="";
-    var objMedium = "";
-    var objDept = "";
-    var objBegin = "";
-    var objEnd = "";
-    var objArtistBegin = "";
-    var objArtistEnd = "";
-    var objName = "";
-    var objCulture = "";
-    var objPeriod = "";
-    var objDynasty = "";
-    var objReign = "";
-    var objDimensions = "";
-    var objCreditLine = "";
-    getMetObject(obj_ID,function(item){
-        objTitle = item.title;
-        objPrimaryImage = item.primaryImageSmall;
-        objArtistDisplayName = item.artistDisplayName;
-        objMedium = item.medium;
-        objDept = item.department;
-        objBegin = item.objectBeginDate;
-        objEnd = item.objectEndDate;
-        objArtistBegin = item.artistBeginDate;
-        objArtistEnd = item.artistEndDate;
-        objName = item.objectName;
-        objCulture = item.culture;
-        objPeriod = item.period;
-        objDynasty = item.dynasty;
-        objReign = item.reign;
-        objDimensions = item.dimensions;
-        objCreditLine = item.creditLine;
-        document.getElementById("metArt").innerHTML += obj_ID + ": "+ objTitle +" <br>";
-        document.getElementById("metArt").innerHTML += objName +" <br>";
-        document.getElementById("metArt").innerHTML += "<img src="+ objPrimaryImage +" alt="+objTitle+"\"> <br>";
-        document.getElementById("metArt").innerHTML += "artist: " + objArtistDisplayName +" <br>";
-        if (objArtistBegin.length > 0 ) {
-            document.getElementById("metArt").innerHTML += "artist birth: " + objArtistBegin +" death: "+objArtistEnd+ "<br>";
-        }
-        document.getElementById("metArt").innerHTML += "medium: " + objMedium +" <br>";
-        document.getElementById("metArt").innerHTML += "department: " + objDept +" <br>";
-        if (objCulture.length > 0) {
-            document.getElementById("metArt").innerHTML += "culture: " + objCulture +" <br>";
-        }
-        if (objPeriod.length > 0){
-            document.getElementById("metArt").innerHTML += "period: " + objPeriod +" <br>";
-        }
-        if (objDynasty.length > 0){
-            document.getElementById("metArt").innerHTML += "dynasty: " + objDynasty +" <br>";
-        }
-        if (objReign.length > 0){
-            document.getElementById("metArt").innerHTML += "reign: " + objReign +" <br>";
-        }
-        if (objDimensions.length > 0){
-            document.getElementById("metArt").innerHTML += "artwork dimensions: " + objDimensions +" <br>";
-        }        
-        document.getElementById("metArt").innerHTML += "object begin date: " + objBegin + " object end date: "+ objEnd + " <br>";
-        if (objCreditLine.length > 0){
-            document.getElementById("metArt").innerHTML += "origin and year acquired: " + objCreditLine +" <br>";
-        }         
-        document.getElementById("metArt").innerHTML += "<hr>";
-    });
-};
+  var objTitle = "";
+  var objPrimaryImage = "";
+  var objArtistDisplayName = "";
+  var objMedium = "";
+  var objDept = "";
+  var objBegin = "";
+  var objEnd = "";
+  var objArtistBegin = "";
+  var objArtistEnd = "";
+  var objName = "";
+  var objCulture = "";
+  var objPeriod = "";
+  var objDynasty = "";
+  var objReign = "";
+  var objDimensions = "";
+  var objCreditLine = "";
+  var objConstituents = [];
+
+  getMetObject(obj_ID, function (item) {
+    objTitle = item.title;
+    objPrimaryImage = item.primaryImageSmall;
+    objArtistDisplayName = item.artistDisplayName;
+    objMedium = item.medium;
+    objDept = item.department;
+    objBegin = item.objectBeginDate;
+    objEnd = item.objectEndDate;
+    objArtistBegin = item.artistBeginDate;
+    objArtistEnd = item.artistEndDate;
+    objName = item.objectName;
+    objCulture = item.culture;
+    objPeriod = item.period;
+    objDynasty = item.dynasty;
+    objReign = item.reign;
+    objDimensions = item.dimensions;
+    objCreditLine = item.creditLine;
+    objConstituents = item.constituents;
+    objWiki = item.objectWikidata_URL;
+
+    document.getElementById("metArt").innerHTML +=
+      obj_ID + ": " + objTitle + " <br>";
+    document.getElementById("metArt").innerHTML += objName + " <br>";
+    document.getElementById("metArt").innerHTML +=
+      "<img src=" + objPrimaryImage + " alt=" + objTitle + '"> <br>';
+    document.getElementById("metArt").innerHTML +=
+      "artist: " + objArtistDisplayName + " <br>";
+    if (objArtistBegin.length > 0) {
+      document.getElementById("metArt").innerHTML +=
+        "artist birth: " + objArtistBegin + " death: " + objArtistEnd + "<br>";
+    }
+    document.getElementById("metArt").innerHTML +=
+      "medium: " + objMedium + " <br>";
+    document.getElementById("metArt").innerHTML +=
+      "department: " + objDept + " <br>";
+    if (objCulture.length > 0) {
+      document.getElementById("metArt").innerHTML +=
+        "culture: " + objCulture + " <br>";
+    }
+    if (objPeriod.length > 0) {
+      document.getElementById("metArt").innerHTML +=
+        "period: " + objPeriod + " <br>";
+    }
+    if (objDynasty.length > 0) {
+      document.getElementById("metArt").innerHTML +=
+        "dynasty: " + objDynasty + " <br>";
+    }
+    if (objReign.length > 0) {
+      document.getElementById("metArt").innerHTML +=
+        "reign: " + objReign + " <br>";
+    }
+    if (objDimensions.length > 0) {
+      document.getElementById("metArt").innerHTML +=
+        "artwork dimensions: " + objDimensions + " <br>";
+    }
+
+    document.getElementById("metArt").innerHTML +=
+      "object begin date: " +
+      objBegin +
+      " object end date: " +
+      objEnd +
+      " <br>";
+
+    if (objCreditLine.length > 0) {
+      document.getElementById("metArt").innerHTML +=
+        "origin and year acquired: " + objCreditLine + " <br>";
+    }
+
+    if (objConstituents.length > 0 ) {
+        document.getElementById("metArt").innerHTML +=
+        "constituents: " + objConstituents + " <br>";
+    }
+    if (objWiki.length > 0 ) {
+        document.getElementById("metArt").innerHTML +=
+        `WIKIData: <a href="${objWiki} target="_blank" title="WIKIData link">WIKI link</a>  <br>`;
+    } 
+
+    document.getElementById("metArt").innerHTML += "<hr>";
+  });
+}
 
 function getSelection() {
-    $(document).ready(function(){
-        $("#searchBtn").on("click",function() {
-            document.getElementById("metCriteria").innerHTML = "";
-            writeSelection();
-        });
-    });  
-};
+  $(document).ready(function () {
+    $("#searchBtn").on("click", function () {
+      document.getElementById("metCriteria").innerHTML = "";
+      writeSelection();
+    });
+  });
+}
 
 function writeSelection() {
-    qryStr = document.forms["metArtCriteria"]["queryString"].value;
-    qryDept = document.forms["metArtCriteria"]["qryDept"].value;
-    qryHighlight = document.forms["metArtCriteria"]["qryHighlight"].value;
-    qryView = document.forms["metArtCriteria"]["qryView"].value;        // isOnView
-    qryCult = document.forms["metArtCriteria"]["qryCult"].value;       // artistOrCulture
-    qryMedium = document.forms["metArtCriteria"]["qryMedium"].value;     // medium
-    qryImages = document.forms["metArtCriteria"]["qryImages"].value;       // hasImages
-    qryLoc = document.forms["metArtCriteria"]["qryLoc"].value;          // geoLocation
-// must have both values for dateBegin and dateEnd queries:
-    qryBegin = document.forms["metArtCriteria"]["qryBegin"].value;        // dateBegin
-    qryEnd = document.forms["metArtCriteria"]["qryEnd"].value;    
+  document.getElementById("metCriteria").innerHTML = "";
+  qryStr = document.forms["metArtCriteria"]["queryString"].value;
+  qryDept = document.forms["metArtCriteria"]["qryDept"].value;
+  qryHighlight = document.forms["metArtCriteria"]["qryHighlight"].value;
+  qryView = document.forms["metArtCriteria"]["qryView"].value; // isOnView
+  qryCult = document.forms["metArtCriteria"]["qryCult"].value; // artistOrCulture
+  qryMedium = document.forms["metArtCriteria"]["qryMedium"].value; // medium
+  qryImages = document.forms["metArtCriteria"]["qryImages"].value; // hasImages
+  qryLoc = document.forms["metArtCriteria"]["qryLoc"].value; // geoLocation
+  // must have both values for dateBegin and dateEnd queries:
+  qryBegin = document.forms["metArtCriteria"]["qryBegin"].value; // dateBegin
+  qryEnd = document.forms["metArtCriteria"]["qryEnd"].value;
 
-    let criteriaString = `Selection: ${qryStr} Department: ${qryDept} highlighted: ${qryHighlight} on view: ${qryView}
+  getDeptName(qryDept);
+  /*
+        So we have a set of selected criteria, but also unselected or blank criteria from the form
+        Need to strip out the blank selection qualifiers
+  */
+
+  searchCrit1 = `departmentId=${qryDept}&q=${qryStr}`;
+  var searchCrit2Orig = `isHighlight=${qryHighlight}&isOnView=${qryView}&hasImages=${qryImages}&geoLocation=${qryLoc}&dateBegin=${qryBegin}&dateEnd=${qryEnd}`;
+  searchCritArray = searchCrit2Orig.split("&");
+  searchCrit2 = stripBlankSelections(searchCritArray);
+  document.getElementById("metCriteria").innerHTML += `<p> Search criteria: ${searchCrit1} & ${searchCrit2} </p>`;
+
+  /* let criteriaString = `Selection: ${qryStr} Department: ${qryDept} highlighted: ${qryHighlight} on view: ${qryView}
         artist or culture: ${qryCult} medium: ${qryMedium} has images: ${qryImages} geographic location: ${qryLoc} 
         work date began: ${qryBegin} work date finished: ${qryEnd}`;
-    document.getElementById("metCriteria").innerHTML = "<p> " + criteriaString + "</p>";
-    //document.getElementById("metCriteria").innerHTML += " Department: "+this.qryDept+" ";
+  document.getElementById("metCriteria").innerHTML =    "<p> " + criteriaString + "</p>";
+  */
+}
 
-    getDeptName(qryDept);  
-
-    searchCrit1 = `departmentId=${qryDept}&q=${qryStr}`;
-    searchCrit2 = `isHighlight=${qryHighlight}&isOnView=${qryView}&hasImages=${qryImages}&geoLocation=${qryLoc}&dateBegin=${qryBegin}&dateEnd=${qryEnd}`;
-};
+function stripBlankSelections(searchCritArray) {
+  var searchString = ""; 
+  for (let i in searchCritArray) {
+    if (searchCritArray[i].length != searchCritArray[i].lastIndexOf("=") + 1) {
+      searchString += "&" + searchCritArray[i];
+    }
+  }
+  return searchString;
+}
